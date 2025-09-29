bc674f670b3adeabcb2a0dd4c82453b8d1ecd102
cbriggs-code <c.briggs@jcsds.net>
2025-09-29 16:25:20 -0700

    Update NormalDriveOp.java

--- NormalDriveOp.java
+++ NormalDriveOp.java
@@ -1,3 +1,11 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 // so like just import this stuff later.....
 public class Normaldrive extends LinearOpMode {
     DcMotor backLeftDrive;
@@ -34,4 +42,5 @@ public class Normaldrive extends LinearOpMode {
     }
 
 }
+
 
