3d94ff8c7d00e0f031de48a3e6b6adbc24bcb0c7
theelementains-droid <theelementains@gmail.com>
2025-10-20 16:18:30 -0700

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -8,9 +8,33 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 // so like just import this stuff later.....
 public class Normaldrive extends LinearOpMode {
-    DcMotor backLeftDrive;
-    DcMotor backRightDrive;
-    DcMotor frontLeftDrive;
-    DcMotor frontRightDrive;
+    DcMotor m1;
+    DcMotor m2;
+    DcMotor m3;
+    DcMotor m4;
     DcMotor powerDrive;
     BNO055IMU imu;
+
+@Override
+    public void runOpMode() {
+      backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+      backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+      frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+      frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+      powerDrive = hardwareMap.get(DcMotor.class, "powerDrive");
+      imu = hardwareMap.get(BNO055IMU.class, "imu");
+      backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+      frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+      Boolean thelongdriveback = false;
+      while (opModeIsActive()) {
+          // need to replace this stuff with the dead wheel encoders
+        telemetry.addData("left", backLeftDrive.getCurrentPosition());
+        telemetry.addData("right", backRightDrive.getCurrentPosition());
+        telemetry.addData("top", frontLeftDrive.getCurrentPosition());
+        telemetry.update();
+            powerDrive.setPower(1);
+        }
+      }
+    }
+
+}
