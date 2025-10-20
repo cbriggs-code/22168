84bf32d1cc619d0dfce522a12e578a7fca7eb851
theelementains-droid <theelementains@gmail.com>
2025-10-20 16:13:10 -0700

    Create Auto.java

---
+++ Auto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+// so like just import this stuff later.....
+public class Normaldrive extends LinearOpMode {
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor powerDrive;
+    BNO055IMU imu;
